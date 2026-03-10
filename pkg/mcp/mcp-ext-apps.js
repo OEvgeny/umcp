16428850f2f24c1a29f24b09336399275c8de68b
Eugene <EOlonov@gmail.com>
2026-03-09 23:35:57 -0700

    Updated mcp-ext-apps.js

--- pkg/mcp/mcp-ext-apps.js
+++ pkg/mcp/mcp-ext-apps.js
@@ -1,4 +1,4 @@
 export * from '../../node_modules/@modelcontextprotocol/ext-apps/src/app.ts';
 export * from '../../node_modules/@modelcontextprotocol/ext-apps/src/app-bridge.ts';
-export * from '../node_modules/@modelcontextprotocol/ext-apps/src/server/index.ts'; 
+export * from '../node_modules/@modelcontextprotocol/ext-apps/src/server/index.ts';
 export { MessagePortTransport } from './src/message-transport.js';
