cb02ccf889f880be0c1c4ad92590bc73d6050901
Eugene <EOlonov@gmail.com>
2026-03-09 23:17:20 -0700

    Fix zod (#4)
    * Updated index.js
    
    * Updated releases.yml
    
    * Add package stamp
    
    ---------
    
    Co-authored-by: Eugene Olonov <v-eolonov@microsoft.com>

--- pkg/zod-compat/index.js
+++ pkg/zod-compat/index.js
@@ -1,3 +1,4 @@
 export { safeParse } from '../../node_modules/zod/v4/core/index.js';
 export { fromJSONSchema } from '../../node_modules/zod/v4/classic/from-json-schema.js';
 export * from './z.js';
+export * as z from './z.js';
