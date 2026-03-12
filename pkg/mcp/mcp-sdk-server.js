40975e467b43920ffd04d84f55dc5491b3565691
Eugene <EOlonov@gmail.com>
2026-03-11 18:12:20 -0700

    Updated mcp-sdk-server.js

--- pkg/mcp/mcp-sdk-server.js
+++ pkg/mcp/mcp-sdk-server.js
@@ -1,3 +1,3 @@
 export * from '@modelcontextprotocol/sdk/server/index.js';
 export * from '@modelcontextprotocol/sdk/server/mcp.js';
-export * from '@modelcontextprotocol/sdk/server/webStreamableHttp.js';
+export * from '../../node_modules/@modelcontextprotocol/sdk/server/webStreamableHttp.js';
